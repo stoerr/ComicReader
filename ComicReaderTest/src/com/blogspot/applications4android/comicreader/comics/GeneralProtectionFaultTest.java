486cb3cac0468b03a2c28e7b76eb494d403cbe60
Hans-Peter Stoerr <Hans-Peter.Stoerr@t-systems-mms.com>
2017-02-15 10:56:29 +0100

    EerieCutiesTest.java, GeneralProtectionFaultTest.java

--- ComicReaderTest/src/com/blogspot/applications4android/comicreader/comics/GeneralProtectionFaultTest.java
+++ ComicReaderTest/src/com/blogspot/applications4android/comicreader/comics/GeneralProtectionFaultTest.java
@@ -4,21 +4,17 @@ import com.blogspot.applications4android.comicreader.core.Strip;
 import junit.framework.TestCase;
 
 /**
- * Created by hps on 08.06.2016.
+ * Created by hps on 01.02.2017.
  */
-public class EerieCutiesTest extends TestCase {
+public class GeneralProtectionFaultTest extends TestCase {
 
     public void test() throws Exception {
 
-        EerieCuties comic = new EerieCuties();
-        String ls = comic.getLatestStripUrl();
-        assertNotNull(ls);
-        System.out.println(ls);
+        GeneralProtectionFault comic = new GeneralProtectionFault();
         Strip strip = comic.getLatestStrip();
         assertNotNull(strip);
         String image = strip.getImage(comic);
         assertNotNull(image);
         System.out.println(image);
     }
-
 }
